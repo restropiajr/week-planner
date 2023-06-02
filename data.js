0b4dbf06b5752d8819a6b504d2b475cbe236c4e2
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-02 18:32:50 +0000

    Fixed css styling and main.js

---
+++ data.js
@@ -0,0 +1,19 @@
+var data = {
+  view: 'entry-form',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+window.addEventListener('beforeunload', event => {
+
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('data', dataJSON);
+
+});
+
+const previousDataJSON = localStorage.getItem('data');
+
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const $entryBtn = document.querySelector('.entry-btn');
-const $submitBtn = document.querySelector('.submit-btn');
 const $modalContainer = document.querySelector('.modal-container');
+const $entryForm = document.querySelector('.entry-Form');
 
 $entryBtn.addEventListener('click', function () {
   $modalContainer.classList.remove('hidden');
 });
 
-$submitBtn.addEventListener('click', function () {
+$entryForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  // create entry for new object
+
   $modalContainer.classList.add('hidden');
+
 });
+
+// render entry function
